480f06fb7851b5178a928a1e88d44c7a1c6d0bd0
vvdiduh <vvdiduh@gmail.com>
2023-02-08 23:29:54 +0200

    finish task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Statistics from './Statistics';
-import { Title } from './App.styled';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+import Section from './Section';
 
 class App extends Component {
   state = {
-    good: 2,
-    neutral: 1,
-    bad: 3,
+    good: 0,
+    neutral: 3,
+    bad: 0,
   };
 
   countTotalFeedback = (good, neutral, bad) => {
@@ -17,27 +19,39 @@ class App extends Component {
     return Math.round((good * 100) / (good + neutral + bad));
   };
 
+  addFeedback = feedback => {
+    this.setState(prevState => {
+      return {
+        [feedback]: prevState[feedback] + 1,
+      };
+    });
+  };
+
   render() {
-    // const { good } = this.state;
-    // const { neutral } = this.state;
-    // const { bad } = this.state;
+    const { good } = this.state;
+    const { neutral } = this.state;
+    const { bad } = this.state;
 
     return (
-      <>
-        <Title>Будь ласка залиште відгук</Title>
-        <ul>
-          <li>
-            <button>Неймовірно</button>
-          </li>
-          <li>
-            <button>Добре</button>
-          </li>
-          <li>
-            <button>Можна краще</button>
-          </li>
-        </ul>
-        <Statistics />
-      </>
+      <Section title="Будь ласка залиште відгук">
+        <FeedbackOptions
+          options={this.state}
+          onLeaveFeedback={this.addFeedback}
+        />
+        {this.countTotalFeedback(good, neutral, bad) > 0 ? (
+          <>
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          </>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     );
   }
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
+import './Statistcs.styled';
 
-class Statistics extends Component {
-  render() {
-    return (
-      <>
-        <h2> Статистика </h2>
-        <ul>
-          <li>
-            <p>Неймовірно: {good}</p>
-          </li>
-          <li>
-            <p>Добре: {neutral}</p>
-          </li>
-          <li>
-            <p>Можна краще: {bad}</p>
-          </li>
-          <li>
-            <p>
-              Кількість відгуків: {this.countTotalFeedback(good, neutral, bad)}
-            </p>
-          </li>
-          <li>
-            <p>
-              Кількість позитивних відгуків:{' '}
-              {this.countPositiveFeedbackPercentage(good, neutral, bad)}%
-            </p>
-          </li>
-        </ul>
-      </>
-    );
-  }
-}
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <>
+    <h2> Статистика </h2>
+    <ul>
+      <li>
+        <p>Неймовірно: {good}</p>
+      </li>
+      <li>
+        <p>Добре: {neutral}</p>
+      </li>
+      <li>
+        <p>Можна краще: {bad}</p>
+      </li>
+      <li>
+        <p>Кількість відгуків: {total(good, neutral, bad)}</p>
+      </li>
+      <li>
+        <p>
+          Кількість позитивних відгуків:{' '}
+          {positivePercentage(good, neutral, bad)} %
+        </p>
+      </li>
+    </ul>
+  </>
+);
+
 export default Statistics;
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.func.isRequired,
+  positivePercentage: PropTypes.func.isRequired,
+};
